515adb3adb4658efa5e9b282b77387734fa41087
Abajit Dey <abajitdeyag10118@gmail.com>
2023-06-17 23:05:57 +0530

    Updated design or cards
    Updated design of cards UI and added text color and button color in Button component

--- src/components/StyledComponents/Button.tsx
+++ src/components/StyledComponents/Button.tsx
@@ -1,9 +1,10 @@
 import { css } from '@emotion/css';
-import { getColor } from '../../core/bearStore';
 
 interface ButtonProps {
   children: any;
   type: any;
+  textColor: string;
+  buttonColor: string;
 }
 
 export default function Button(props: ButtonProps) {
@@ -11,10 +12,10 @@ export default function Button(props: ButtonProps) {
     <button className={css`
       border: none;
       padding: 0.7rem 1.2rem;
-      color: ${getColor('button')};
+      color: ${props.textColor};
       border-radius: 6px;
       cursor: pointer;
-      background-color: ${getColor('primary')};
+      background-color: ${props.buttonColor};
     `} type={props.type}>
       {props.children}
     </button>
--- src/components/UserInterface/UserInterface.tsx
+++ src/components/UserInterface/UserInterface.tsx
@@ -1,4 +1,7 @@
 import { Button, HighlightedText } from '../StyledComponents';
+import { css } from '@emotion/css';
+import { IoCheckmarkCircleOutline } from 'react-icons/io5';
+import { getColor } from '../../core/bearStore';
 import './UserInterface.scss';
 
 export default function UserInterface() {
@@ -16,7 +19,13 @@ export default function UserInterface() {
           </ul>
         </div>
         <div className="signup-btn-wrapper">
-          <Button>Get Started</Button>
+          <Button
+            textColor={getColor('button')}
+            buttonColor={getColor('primary')}
+            type="submit"
+          >
+            Get Started
+          </Button>
         </div>
       </nav>
       <main className="section">
@@ -30,8 +39,14 @@ export default function UserInterface() {
             visualize your palette even before designing your next app idea.
           </p>
           <div className="email-input-wrapper">
-            <input placeholder='Enter your email address' />
-            <Button type="submit">Subscribe</Button>
+            <input placeholder="Enter your email address" />
+            <Button
+              textColor={getColor('button')}
+              buttonColor={getColor('primary')}
+              type="submit"
+            >
+              Subscribe
+            </Button>
           </div>
         </div>
         <div className="image-wrapper">
@@ -40,6 +55,220 @@ export default function UserInterface() {
           </div>
         </div>
       </main>
+      <section
+        id="pricing"
+        className="pricing"
+        style={{ backgroundColor: '#E1E5E1' }}
+      >
+        <div className="header">
+          <h2>Choose the plan that fits your needs</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis massa
+            nibh, consequat id mi consectetur, porttitor pellentesque dolor.
+          </p>
+        </div>
+        <div className="cards-wrapper">
+          <div
+            className={`price-card ${css`
+              background-color: #eeeeee;
+            `}`}
+          >
+            <div className="card-header">
+              <span
+                className={css`
+                  background-color: transparent;
+                  border-radius: 100px;
+                  border: solid 1px #787878;
+                  padding: 5px 13px;
+                  font-size: 13px;
+                  color: #787878;
+                `}
+              >
+                Personal
+              </span>
+            </div>
+            <div className="card-body">
+              <div className="price-wrapper">
+                <span
+                  className="price"
+                  style={{ color: `${getColor('primary')}` }}
+                >
+                  Free
+                </span>
+                <span className="price-period" style={{ color: '#787878' }}>
+                  for life
+                </span>
+              </div>
+              <p className="description" style={{ color: '#787878' }}>
+                Everything you’ll need to get started on that idea, you’ve been
+                waiting all this time
+              </p>
+            </div>
+            <ul className="features-list" style={{ color: '#787878' }}>
+              <li>
+                <IoCheckmarkCircleOutline className="icon" />
+                <span>Lorem ipsum</span>
+              </li>
+              <li>
+                <IoCheckmarkCircleOutline className="icon" />
+                <span>Etiam at dolor et turpis mattis</span>
+              </li>
+              <li>
+                <IoCheckmarkCircleOutline className="icon" />
+                <span>Phasellus mattis porttitor</span>
+              </li>
+              <li>
+                <IoCheckmarkCircleOutline className="icon" />
+                <span>Integer suscipit dolor a tortor</span>
+              </li>
+            </ul>
+            <div className="card-footer">
+              <Button
+                textColor={getColor('button')}
+                buttonColor={getColor('primary')}
+                type="button"
+              >
+                Get started
+              </Button>
+            </div>
+          </div>
+          <div
+            className="price-card"
+            style={{ backgroundColor: `${getColor('primary')}` }}
+          >
+            <div className="card-header">
+              <span
+                className={css`
+                  background-color: transparent;
+                  border-radius: 100px;
+                  border: solid 1px #a2a2a2;
+                  padding: 5px 13px;
+                  font-size: 13px;
+                  color: #a2a2a2;
+                `}
+              >
+                Personal Pro
+              </span>
+            </div>
+            <div className="card-body">
+              <div className="price-wrapper">
+                <span
+                  className={css`
+                    color: ${getColor('button')};
+                  `}
+                >
+                  $
+                </span>
+                <span
+                  className="price"
+                  style={{ color: `${getColor('button')}` }}
+                >
+                  20
+                </span>
+                <span className="price-period" style={{ color: '#A2A2A2' }}>
+                  per month
+                </span>
+              </div>
+              <p className="description" style={{ color: '#A2A2A2' }}>
+                With additional features, this is the best option for
+                individuals and professionals or a small team
+              </p>
+            </div>
+            <ul className="features-list" style={{ color: '#A2A2A2' }}>
+              <li>
+                <IoCheckmarkCircleOutline className="icon" />
+                <span>Lorem ipsum</span>
+              </li>
+              <li>
+                <IoCheckmarkCircleOutline className="icon" />
+                <span>Etiam at dolor et turpis mattis</span>
+              </li>
+              <li>
+                <IoCheckmarkCircleOutline className="icon" />
+                <span>Phasellus mattis porttitor</span>
+              </li>
+              <li>
+                <IoCheckmarkCircleOutline className="icon" />
+                <span>Integer suscipit dolor a tortor</span>
+              </li>
+            </ul>
+            <div className="card-footer">
+              <Button
+                textColor={getColor('primary')}
+                buttonColor={getColor('button')}
+                type="button"
+              >
+                Get started
+              </Button>
+            </div>
+          </div>
+          <div
+            className={`price-card ${css`
+              background-color: #eeeeee;
+            `}`}
+          >
+            <div className="card-header">
+              <span
+                className={css`
+                  background-color: transparent;
+                  border-radius: 100px;
+                  border: solid 1px #787878;
+                  padding: 5px 13px;
+                  font-size: 13px;
+                  color: #787878;
+                `}
+              >
+                Teams
+              </span>
+            </div>
+            <div className="card-body">
+              <div className="price-wrapper">
+                <span>$</span>
+                <span
+                  className="price"
+                  style={{ color: `${getColor('primary')}` }}
+                >
+                  50
+                </span>
+                <span className="price-period" style={{ color: '#787878' }}>
+                  per month
+                </span>
+              </div>
+              <p className="description" style={{ color: '#787878' }}>
+                Everything you’ll need to get started on that idea, you’ve been
+                waiting all this time
+              </p>
+            </div>
+            <ul className="features-list" style={{ color: '#787878' }}>
+              <li>
+                <IoCheckmarkCircleOutline className="icon" />
+                <span>Lorem ipsum</span>
+              </li>
+              <li>
+                <IoCheckmarkCircleOutline className="icon" />
+                <span>Etiam at dolor et turpis mattis</span>
+              </li>
+              <li>
+                <IoCheckmarkCircleOutline className="icon" />
+                <span>Phasellus mattis porttitor</span>
+              </li>
+              <li>
+                <IoCheckmarkCircleOutline className="icon" />
+                <span>Integer suscipit dolor a tortor</span>
+              </li>
+            </ul>
+            <div className="card-footer">
+              <Button
+                textColor={getColor('button')}
+                buttonColor={getColor('primary')}
+                type="button"
+              >
+                Get started
+              </Button>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
